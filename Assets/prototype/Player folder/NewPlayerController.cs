dbab976827278c7cb36eb7727e9d331b33c88dc4
Daniel <drolon01@hotmail.com>
2024-10-09 22:02:17 +0100

    Smooth landing and orientation finished!
    need to fix movement

--- Assets/prototype/Player folder/NewPlayerController.cs
+++ Assets/prototype/Player folder/NewPlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -10,15 +11,18 @@ public class NewPlayerController : MonoBehaviour
     [Header ("Player components")]
     [SerializeField] Rigidbody rb;
     [SerializeField] Animator playerAni;
+    [SerializeField] ConstantForce myGravity; 
+    [SerializeField] Camera myCamera;
+    [SerializeField] GameObject myCameraOrientation;
+    [SerializeField] CinemachineFreeLook myCameraCM;
 
     [Header("Movement Variables")]
-    [SerializeField] ConstantForce myGravity;
-    [Tooltip("The total gravitational pull that the player will have in any direction")]
     [SerializeField] float gravityForce;
     [SerializeField] float moveSpeed;
     [SerializeField] float maxSpeedWalk;
     [SerializeField] float deceleration;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float cameraRotationSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] float groundedDrag;
     [SerializeField] float zeroGravDrag;
@@ -26,9 +30,12 @@ public class NewPlayerController : MonoBehaviour
     [Header("Debug elements to inspect")]
     [SerializeField] Vector2 moveInput;
     [SerializeField] Vector3 gravitationalRotation;
+    [SerializeField] Vector3 downDirection;
+    [SerializeField] int presslogged;
+    
 
-    [Header("Assets required")]
-    [SerializeField] Camera myCamera;
+    //[Header("Assets required")]
+
 
     [Header("Player States")]
     [SerializeField] bool zerograv;
@@ -36,6 +43,8 @@ public class NewPlayerController : MonoBehaviour
     [SerializeField] bool immobile;
     [SerializeField] bool grounded;
     [SerializeField] bool diving;
+    [SerializeField] bool shiftDiving; //to be used only when toggling out from zero gravity, in order to smooth landing.
+    
 
     #endregion
 
@@ -45,15 +54,32 @@ public class NewPlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         myCamera = FindFirstObjectByType<Camera>();
+        downDirection = transform.TransformDirection(Vector3.down);
     }
 
     // Update is called once per frame
     void Update()
     {
-        IsGrounded();
-        if(!grounded && !diving)
+        if (!zerograv)
         {
-            HighGround();
+            if (shiftDiving)
+            {
+                SmoothLanding();
+            }
+            else
+            {
+                IsGrounded();
+                if (!grounded && !diving)
+                {
+                    HighGround();
+                }
+            }
+
+        }
+        //otherchecks!
+        if(myCameraCM.m_YAxisRecentering.m_enabled == true && myCameraCM.m_YAxis.Value == 0.5f)
+        {
+            myCameraCM.m_YAxisRecentering.m_enabled = false;
         }
     }
 
@@ -104,14 +130,60 @@ public class NewPlayerController : MonoBehaviour
             }
         }
     }
+    //Here go unique events that are triggered sometimes
+    #region ReactionEvents
+    void OrientatePlayer(Vector3 gravityDirection)
+    {
+        Debug.Log("Updating");
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, -gravityDirection) * transform.rotation;
 
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        /*
+        targetRotation = Quaternion.FromToRotation(myCameraOrientation.transform.up, transform.up) * myCameraOrientation.transform.rotation;
+        myCameraOrientation.transform.rotation = Quaternion.Slerp(myCameraOrientation.transform.rotation,targetRotation, cameraRotationSpeed * Time.deltaTime);
+        */
+
+    }
+    void SmoothLanding()
+    {
+        RaycastHit hit;
+        float rayLength = 1.1f;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * rayLength, Color.green);
+        if (Physics.Raycast(transform.position, -transform.up, out hit, rayLength))
+        {
+            //character snapping to surface
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            //gravity adjusting to surface
+            Vector3 newGravity = -transform.up.normalized * gravityForce;
+            myGravity.force = newGravity;
+
+            //camera orientation
+            SmoothCamera();
+            grounded = true;
+            shiftDiving = false;
+            Debug.Log("fixed orientation");
+        }
+    }
+    void SmoothCamera()
+    {
+        //camera orientation
+        Quaternion targetRotation = Quaternion.FromToRotation(myCameraOrientation.transform.up, transform.up) * myCameraOrientation.transform.rotation;
+        Debug.Log(targetRotation);
+        myCameraOrientation.transform.rotation = Quaternion.Slerp(myCameraOrientation.transform.rotation, targetRotation, cameraRotationSpeed * Time.deltaTime);
+        myCameraCM.m_YAxisRecentering.m_enabled = true;
+        myCameraCM.m_YAxisRecentering.RecenterNow();
+    }
+    #endregion
+    //Here goes recurring events that go in update
     #region StatusChecks
     void IsGrounded()
     {
         RaycastHit hit;
         float rayLength = 1.1f; // Adjust based on your character's size
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * rayLength, Color.green);
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayLength))
+        if (Physics.Raycast(transform.position, /*downDirection,*/ -transform.up, out hit, rayLength))
         {
             if (diving)
             {
@@ -137,8 +209,8 @@ public class NewPlayerController : MonoBehaviour
     {
         RaycastHit hit;
         float rayLength = 15f; // Adjust based on your character's size
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * rayLength, Color.red);
-        if (!Physics.Raycast(transform.position, Vector3.down, out hit, rayLength))
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * rayLength, Color.red);
+        if (!Physics.Raycast(transform.position, /*downDirection*/ -transform.up, out hit, rayLength))
         {
             diving = true;
             Debug.Log("Flying");
@@ -155,26 +227,33 @@ public class NewPlayerController : MonoBehaviour
     #region InputEvents
     public void ToggleGrav(InputAction.CallbackContext context)
     {
-        shifted = true;
-        if(!zerograv)
+        if(context.started)
         {
-            myGravity.force = new Vector3 (0, 0, 0);
-            zerograv = true;
-            immobile = true;
-            rb.freezeRotation = false;
-            rb.drag = zeroGravDrag;
+            presslogged++;
+            shifted = true;
+            if (!zerograv)
+            {
+                myGravity.force = new Vector3(0, 0, 0);
+                zerograv = true;
+                immobile = true;
+                grounded = false;
+                rb.freezeRotation = false;
+                rb.drag = zeroGravDrag;
+            }
+            else
+            {
+                Vector3 cameraDirection = myCamera.transform.forward;
+                OrientatePlayer(cameraDirection);
+                Vector3 newGravity = cameraDirection.normalized * gravityForce;
+                myGravity.force = newGravity;
+                zerograv = false;
+                immobile = false;
+                shiftDiving = true;
+                rb.freezeRotation = true;
+                rb.drag = 0f;
+            }
         }
-        else
-        {
-            Vector3 cameraDirection = myCamera.transform.forward;
-            Vector3 newGravity = cameraDirection.normalized * gravityForce;
-            myGravity.force = newGravity;
-            zerograv = false;
-            immobile = false;
-            grounded = false;
-            rb.freezeRotation = true;
-            rb.drag = 0f;
-        }
+
     }
     public void Revert(InputAction.CallbackContext context)
     {
@@ -186,6 +265,7 @@ public class NewPlayerController : MonoBehaviour
             rb.freezeRotation = true;
             gameObject.transform.rotation = Quaternion.Euler(0,gameObject.transform.rotation.y,0);
             shifted = false;
+            myCameraOrientation.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
     public void OnMove(InputAction.CallbackContext context)
